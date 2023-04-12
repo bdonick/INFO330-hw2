09a7159594ae8ce6a8e40b465bfd6a55fcf3d6ce
bdonick <121984932+bdonick@users.noreply.github.com>
2023-04-11 21:31:59 -0700

    Update question-6.sql

--- question-6.sql
+++ question-6.sql
@@ -1,1 +1,13 @@
 -- Find tracks (id, name and composer) that are part of a line in an invoice.
+SELECT 
+  tracks.TrackId, 
+  tracks.Name AS TrackName, 
+  tracks.Composer 
+FROM 
+  tracks 
+JOIN 
+  invoice_items ON tracks.TrackId = invoice_items.TrackId 
+GROUP BY 
+  tracks.TrackId 
+ORDER BY 
+  tracks.TrackId;
