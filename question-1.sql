4b709d38baa953ff2f387bb5585f4ca78ac4296c
bdonick <121984932+bdonick@users.noreply.github.com>
2023-04-11 21:24:03 -0700

    Add files via upload

--- question-1.sql
+++ question-1.sql
@@ -1,1 +1,11 @@
 -- Show the customer's full name, Invoice ID, Date of the invoice and billing country of customers who are from Brazil.
+SELECT 
+  invoices.InvoiceId, 
+  invoices.InvoiceDate, 
+  customers.FirstName, 
+  customers.LastName
+FROM 
+  invoices 
+  JOIN customers ON invoices.CustomerId = customers.CustomerId
+WHERE 
+  customers.Country = 'Brazil';
